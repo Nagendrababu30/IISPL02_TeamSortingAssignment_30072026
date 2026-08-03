3956a15e5837171477c0546c57ae044c06ef5ebd
iispl <iispl@iispl>
2026-08-03 12:06:12 +0530

    Implemented ClearingZoneAndCheckAmountComparatorin Main

--- src/com/iispl/main/ChequeSortingApplication.java
+++ src/com/iispl/main/ChequeSortingApplication.java
@@ -171,7 +171,12 @@ public class ChequeSortingApplication {
 	}
 	
 	private static void sortByClearingZoneAndAmount() {
-		
+		 
+		List<Cheque> sortedChequeList=chequeService.sortByClearingZoneAndAmount();
+		System.out.println("ChequeNumber" + "\t" + "AccountNumber" + "\t" + "DrawerName"+ "\t"+"PresentingBank" + "\t"+ "ChequeAmount" + "\t"+"ChequeDate" + "\t"+ "PresentedDate" + "\t"+ "ChequePriority" + "\t"+"ChequeStatus"+ "\t"+ "StatusRemarks"
+				+ "\tClearing Zone");
+		sortedChequeList.forEach(System.out :: println);
+	   
 	}
 
 }
